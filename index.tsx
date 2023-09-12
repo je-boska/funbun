c44fd793a944507e0ce43bbd15643268ec529dcc
je-boska <joneirikboska@gmail.com>
2023-09-12 14:10:05 +0200

    Set up htmx

--- index.tsx
+++ index.tsx
@@ -8,14 +8,22 @@ function page() {
     <html lang='en'>
       <head>
         <title>Fun with bun!</title>
+        <script src='https://unpkg.com/htmx.org@1.9.5'></script>
       </head>
       <body>
         <h1>This is a test of {name}</h1>
+        <button hx-post='/clicked' hx-swap='outerHTML'>
+          Click this
+        </button>
       </body>
     </html>
   );
 }
 
-const app = new Elysia().use(html()).get('/', page).listen(8080);
+const app = new Elysia()
+  .use(html())
+  .get('/', page)
+  .post('/clicked', () => <div>I'm from the server!</div>)
+  .listen(8080);
 
 console.log(`Elysia is running on port ${app.server?.port}`);
