8e1b235e8364a0436b8ee2c716bf67fd232671fb
je-boska <joneirikboska@gmail.com>
2023-09-13 10:30:30 +0200

    Validate body and return in html to client

--- index.tsx
+++ index.tsx
@@ -1,7 +1,5 @@
-import { Elysia } from 'elysia';
+import { Elysia, t } from 'elysia';
 import { html } from '@elysiajs/html';
-
-const name = 'Bun';
 
 function page() {
   return (
@@ -11,10 +9,18 @@ function page() {
         <script src='https://unpkg.com/htmx.org@1.9.5'></script>
       </head>
       <body>
-        <h1>This is a test of {name}</h1>
-        <button hx-post='/clicked' hx-swap='outerHTML'>
-          Click this
-        </button>
+        <form>
+          <input name='test' id='test'></input>
+          <input
+            type='submit'
+            hx-post='/clicked'
+            hx-swap='outerHTML'
+            hx-target='#target'
+          >
+            Submit
+          </input>
+        </form>
+        <div id='target'>Target</div>
       </body>
     </html>
   );
@@ -23,7 +29,17 @@ function page() {
 const app = new Elysia()
   .use(html())
   .get('/', page)
-  .post('/clicked', () => <div>I'm from the server!</div>)
+  .post(
+    '/clicked',
+    ({ body }) => {
+      return <div id='target'>{body.test}</div>;
+    },
+    {
+      body: t.Object({
+        test: t.String(),
+      }),
+    }
+  )
   .listen(8080);
 
 console.log(`Elysia is running on port ${app.server?.port}`);
